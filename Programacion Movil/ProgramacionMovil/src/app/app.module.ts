e976fd6801c8fce4ed43afc29764cea2e98a5b64
unknown <paez94144@gmail.com>
2022-12-05 11:30:00 -0500

    Avance

--- Programacion Movil/ProgramacionMovil/src/app/app.module.ts
+++ Programacion Movil/ProgramacionMovil/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    Component1Component,
+    Component2Component
   ],
   imports: [
     BrowserModule,
